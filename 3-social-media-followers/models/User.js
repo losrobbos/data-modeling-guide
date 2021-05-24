169eaf49e32a8ee222b501085931ff8b51ceb79e
losrobbos <50549469+losrobbos@users.noreply.github.com>
2021-05-24 09:16:27 +0200

    Update User.js

--- 3-social-media-followers/models/User.js
+++ 3-social-media-followers/models/User.js
@@ -7,12 +7,12 @@ const UserSchema = new Schema({
 
   name: { type: String, required: true },
   following: [{ type: Schema.Types.ObjectId, ref: 'User' }], // the people this user follows...
-  followers: [{ type: Schema.Types.ObjectId, ref: 'User' }], // the people who follow this users...
+  followers: [{ type: Schema.Types.ObjectId, ref: 'User' }], // the people who follow this user...
   
   // YES, a model can reference itself, so in one user we can reference other userIds !
   // populating the followers works just as usual: User.findById(...).populate("followers")
     // by default populating does just go ONE level deep, 
-    // so it will not populate again the followers of the follwers (preventing potentially endless populate cycles)
+    // so it will not populate again the followers of the followers or the followers... (preventing potentially endless populate cycles)
 })
 
 const User = model("User", UserSchema)

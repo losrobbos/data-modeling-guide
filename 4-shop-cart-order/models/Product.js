6eab785eb4f4431e0b6bbaa8038b18c9f441e95a
losrobbos <50549469+losrobbos@users.noreply.github.com>
2021-10-21 14:30:41 +0200

    Update Product.js

--- 4-shop-cart-order/models/Product.js
+++ 4-shop-cart-order/models/Product.js
@@ -4,7 +4,7 @@ const { Schema, model } = mongoose
 const ProductSchema = new Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  descrption: { type: String },
+  description: { type: String },
   category: { type: Schema.Types.ObjectId, ref: 'Category' },
 
   // in case you want that products can be listed in MULTIPLE categories - use the line below instead: 
